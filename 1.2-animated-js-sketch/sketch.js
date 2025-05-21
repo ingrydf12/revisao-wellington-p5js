d4e7d2a2b4e8678d588f99bbaf50e26a537ffc39
Ingryd <ingrydcduarte@gmail.com>
2025-05-20 21:47:32 -0300

    complete sketch - animated balls

--- 1.2-animated-js-sketch/sketch.js
+++ 1.2-animated-js-sketch/sketch.js
@@ -1,6 +1,6 @@
 /* Esse arquivo contém o tutorial proposto pelo professor, junto ao desafio no final 
 Ambos presentes no link: https://wellingtonwfsarmento.notion.site/Arrays-e-Bolinhas-Animadas-com-p5-js-Um-Tutorial-1e5a08e68f4080db99a0e212c93b68eb
- * Cada bolinha possui uma posição e uma velocidade armazenadas em arrays.
+ Cada bolinha possui uma posição e uma velocidade armazenadas em arrays.
 */
 
 //MARK: - Estrutura básica de criação de sketch utilizando Javascript Vanilla
@@ -9,12 +9,12 @@ new p5((sketch) => {
   let posicoes = [];
   let velocidades = [];
   let cores = [];
-  const quantBolas = prompt("Quantas bolinhas você quer criar?", 10);
+  const quantBolas = 5;
   let raio = [];
-  const gravidade = sketch.createVector(0, 0.25)
+  const gravidade = sketch.createVector(0, 0.25);
 
   sketch.setup = () => {
-    sketch.createCanvas(600, 400).parent("vetores");
+    sketch.createCanvas(800, 600).parent("vetores");
 
     //Iteração para criar as bolinhas
     for (let i = 0; i < quantBolas; i++) {
@@ -38,40 +38,77 @@ new p5((sketch) => {
       raio.push(sketch.random(10, 30));
     }
   };
-
   sketch.draw = () => {
     sketch.background(15, 15, 15);
     sketch.noStroke();
 
-    for (let i = 0; i < quantBolas; i++) {
+    sketch.fill(255);
+    sketch.textSize(14);
+    sketch.text("Clique para adicionar mais bolinhas", 20, 30);
+
+    for (let i = 0; i < posicoes.length; i++) {
       let pos = posicoes[i];
       let vel = velocidades[i];
 
-      //Aqui adiciona o vetor de velocidade a posição, trazendo a animação da bolinha -> Dentro do loop é uma boa prática
       pos.add(vel);
       vel.add(gravidade);
-      //O QUE FAZ AQUI? - Colisão com as bordas, basicamente -> Pega o x e verifica com a width, assim como y e height.
-      if (
-        posicoes[i].x - raio[i] < 0 ||
-        posicoes[i].x + raio[i] > sketch.width
-      ) {
-        velocidades[i].x = velocidades[i].x * -1;
+
+      posicoes[i].x = sketch.constrain(
+        posicoes[i].x,
+        raio[i],
+        sketch.width - raio[i]
+      );
+      posicoes[i].y = sketch.constrain(
+        posicoes[i].y,
+        raio[i],
+        sketch.height - raio[i]
+      );
+
+      if (posicoes[i].x <= raio[i] || posicoes[i].x >= sketch.width - raio[i]) {
+        velocidades[i].x *= -1;
       }
       if (
-        posicoes[i].y - raio[i] < 0 ||
-        posicoes[i].y + raio[i] > sketch.height
+        posicoes[i].y <= raio[i] ||
+        posicoes[i].y >= sketch.height - raio[i]
       ) {
-        velocidades[i].y = velocidades[i].y * -1;
+        velocidades[i].y *= -1;
       }
 
-      //Colisão entre as bolinhas
-      if (sketch.dist(posicoes[i], posicoes[i + 1]) < raio[i] + raio[i + 1]) {
-        velocidades[i].x *= -1;
-        velocidades[i].y *= -1;
+      for (let j = i + 1; j < posicoes.length; j++) {
+        let distBolas = sketch.dist(
+          posicoes[i].x,
+          posicoes[i].y,
+          posicoes[j].x,
+          posicoes[j].y
+        );
+        if (distBolas < raio[i] + raio[j]) {
+          let temp = velocidades[i].copy();
+          velocidades[i] = velocidades[j].copy();
+          velocidades[j] = temp;
+
+          let overlap = raio[i] + raio[j] - distBolas;
+          let dir = p5.Vector.sub(posicoes[i], posicoes[j]).normalize();
+          posicoes[i].add(dir.copy().mult(overlap / 2));
+          posicoes[j].sub(dir.copy().mult(overlap / 2));
+        }
       }
 
       sketch.fill(cores[i]);
       sketch.ellipse(posicoes[i].x, posicoes[i].y, raio[i] * 2, raio[i] * 2);
     }
+  };
+
+  sketch.mousePressed = () => {
+    cores.push(
+      sketch.color(sketch.random(255), sketch.random(255), sketch.random(255))
+    );
+
+    posicoes.push(sketch.createVector(sketch.mouseX, sketch.mouseY));
+
+    velocidades.push(
+      sketch.createVector(sketch.random(1, 3), sketch.random(1, 3))
+    );
+
+    raio.push(sketch.random(10, 30));
   };
 });
